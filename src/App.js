84960c917882560f3e76f89722880c7f6504b6c5
Zenia Villa <zenia.villa@ibm.com>
2020-01-17 16:13:15 -0800

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import zvLogo from './zv-logo.png';
+
+function App() {
+  return (
+    <div className="main">
+      <div>Welcome!</div>
+      <img src={zvLogo}></img>
+    </div>
+  );
+}
+
+export default App;
