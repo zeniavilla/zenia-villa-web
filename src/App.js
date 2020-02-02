d98d67edc24e3d0ac003c64a615884fc2d04780a
Zenia Villa <zeniavilla@hotmail.com>
2020-02-01 20:53:39 -0800

    Add sass (#2)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 import TopNavigation from './top-navigation/top-navigation';
 
 function App() {
