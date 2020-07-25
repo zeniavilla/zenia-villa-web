edba9cf0c3e95c121f27601c99a115ab8766f2e5
Zenia Villa <zeniavilla@hotmail.com>
2020-07-24 21:06:28 -0700

    Add footer

---
+++ src/footer/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './footer.scss';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div>Written with ♥</div>
+      © Copyright Zenia Villa
+    </div>
+  );
+}
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -7,17 +7,18 @@ import App from "./App";
 import About from './about/about';
 import TopNavigation from './top-navigation/top-navigation';
 import Contact from './contact/contact';
+import Footer from "./footer/footer";
 
 const routing = (
   <Router>
     <div>
       <TopNavigation></TopNavigation>
-      {/* <Breadcrumb></Breadcrumb> */}
       <div className="body">
         <Route exact path="/" component={App}></Route>
         <Route path="/about-me" component={About}></Route>
         <Route path="/contact" component={Contact}></Route>
       </div>
+      <Footer></Footer>
     </div>
   </Router>
 )
