2d3c7e7556dd21f09e15aab8d597b64965b4f3f9
Zenia Villa <zeniavilla@hotmail.com>
2025-12-04 13:59:47 -0800

    Resume page changes

--- src/pages/resume/resume.tsx
+++ src/pages/resume/resume.tsx
@@ -56,8 +56,8 @@ const EXPERIENCE_DATA: ExperienceData[] = [
     title: 'Software Engineer',
     company: 'IBM Aspera',
     body: [
-      "I joined the IBM Aspera team as it was in the beginning stages of Aspera on Cloud (AoC), a SaaS file transfer application. After doing some server-side work for web apps Faspex and Files, I had the opportunity to build features on the client-side and quickly joined the front-end development team. Throughout my time at Aspera, I've moved through the pipeline (pun intended) of fixing small bugs to implementing major features and being an active part of design-development discussions (I love accidental alliteration).",
-      "Recently, the front-end team has expanded into working on Faspex, which involves building new features that fully utilize the Carbon Design System. In a typical week, I'll work in tandem with members of the design, back-end, devtest, and/or microcopy teams. From time to time, I fix critical bugs that may come up in production. I also stay actively involved in design reviews."
+      "I joined the IBM Aspera team as it was in the beginning stages of Aspera on Cloud (AoC), a SaaS file transfer application. After doing some server-side work for web apps Faspex and Files, I had the opportunity to build features on the client-side and quickly joined the frontend development team. Throughout my time at Aspera, I've moved through the pipeline (pun intended) of fixing small bugs to implementing major features and being an active part of design + development discussions (I love accidental alliteration).",
+      "I transitioned into working on Faspex, which I became the application lead for toward the end of my tenure. I was part of an organized effort to build new features that fully utilized the Carbon Design System. This involved more involvement in design reviews as well as fixing critical production bugs reported by customers."
     ],
   },
   {
@@ -66,7 +66,7 @@ const EXPERIENCE_DATA: ExperienceData[] = [
     title: 'Transition',
     company: 'Learning web development',
     body: [
-      "Coding had been on my mind since I took a Computer Science class in high school. After creating the website for AMCT Design using Joomla and customizing the client-side code, I took a few online classes and kept wondering if I could make it a career. My career had taken me down a path that left me feeling unchallenged. With the support of my family, I stopped working and started to learn how to code in JavaScript. I paid for various types of memberships including Code School and Learn.co and found some strucutre to learn JS, Ruby on Rails, and React. It was 8+ days five days a week for seven months and then applying aggressively. I ended up moving to the Bay area after accepting a role at IBM and it ended up being the best opportunity."
+      "Coding has been a passion since high school, which resurfaced after I customized the client-side code for the AMCT Design website. While I enjoyed my previous roles, I felt ready for a new challenge. I decided to fully commit to a career change, pausing my work to dedicate seven months to intensive learning (8+ hours a day). I created a structured path using resources like Code School and Learn.co to master JavaScript, Ruby on Rails, and React. Thankfully it paid off; I transitioned into tech, moving to the Bay Area to join IBM."
     ],
   },
   {
@@ -75,7 +75,7 @@ const EXPERIENCE_DATA: ExperienceData[] = [
     title: 'Marketing Coordinator & Manager',
     company: 'AMCT Design | Giumarra Companies',
     body: [
-      "During this time, I took on various roles in different industries. The highlights include producing tradeshows for an internationally-recognized produce brand, social media strategy and content, creating and managing a startup's website, and sales. A lot of the creativity and being product-minded has carried over into my work as a developer. Moreover, these years helped me realize my love of working closely with people."
+      "During this time, I took on various roles in different industries. Key highlights include producing major tradeshows for an internationally-recognized produce brand, driving social media strategy and content creation, building and managing a startup's website, and developing a solid background in sales. Each experience contributed to my creativity and product-minded thinking, which are skills I actively apply as a developer. Moreover, these years showed me how much I love working closely with people."
     ],
   },
 ];
@@ -137,7 +137,11 @@ function Experience() {
                     {item.timeframe}
                   </span>
                 </div>
-                <p className="text-foreground font-light text-lg leading-relaxed">{item.body}</p>
+                {
+                  item.body.map((para, i) => (
+                    <p className="text-foreground font-light text-lg leading-relaxed" key={`paragraph-${i}`}>{para}</p>
+                  ))
+                }
               </div>
             ))
           }
