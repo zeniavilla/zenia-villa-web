a93abd9a1a552f35929973b4ae3649a3eb6b41c9
Zenia Villa <zeniavilla@hotmail.com>
2025-12-04 20:08:44 -0800

    Github pages naming

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: './',
+  base: '/zenia-villa-web/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
